8f1a142fb5291d6811d0a14d0f920c3aa232539e
Equator <841116931@qq.com>
2021-05-20 10:44:20 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "Equator的个人博客"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
+author = "Equator"
+email = "841116931@qq.com"
 author_homepage = "https://www.imalan.cn"
 description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+key_words = ['Equator', '哒成一派', '博客', '技术']
 language = 'zh-CN'
 external_links = [
     {
